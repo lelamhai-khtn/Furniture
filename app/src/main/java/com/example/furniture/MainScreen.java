1eba3234eb5f19b548dbc860843ce4a25253d883
ADMIN\ADMIN <leelamhair@gmail.com>
2026-08-29 23:37:34 +0700

    base navigationview

--- app/src/main/java/com/example/furniture/MainScreen.java
+++ app/src/main/java/com/example/furniture/MainScreen.java
@@ -1,24 +1,60 @@
 package com.example.furniture;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainScreen extends AppCompatActivity {
-
+    FrameLayout contentFrame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_screen);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+        contentFrame = findViewById(R.id.content_frame);
+        loadFragment(new HomeFragment());
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.bottom_home)
+            {
+                loadFragment(new HomeFragment());
+                return true;
+            }
+            if (id == R.id.bottom_search)
+            {
+                loadFragment(new SearchFragment());
+                return true;
+            }
+            if (id == R.id.bottom_ar)
+            {
+                loadFragment(new ARFragment());
+                return true;
+            }
+            if (id == R.id.bottom_cart)
+            {
+                loadFragment(new CartFragment());
+                return true;
+            }
+            if (id == R.id.bottom_profile)
+            {
+                loadFragment(new ProfileFragment());
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void loadFragment(Fragment fragment){
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_frame,fragment)
+                .commit();
+
     }
 }
